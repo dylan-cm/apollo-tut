0296ca2b1610fa07ee7cf84f43b405bd010fd38a
Dylan Cortez-Modell <dylan.modell@gmail.com>
2021-04-16 16:33:34 -0700

    step 2

--- working/server/src/index.js
+++ working/server/src/index.js
@@ -2,8 +2,22 @@ require('dotenv').config();
 
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
 
-const server = new ApolloServer({typeDefs})
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI,
+    userAPI: new UserAPI({ store }),
+  })
+});
+//* NOTE: If you use this.context in a datasource, it's critical to create a new instance in the dataSources function, rather than sharing a single instance. 
+//* Otherwise, initialize might be called during the execution of asynchronous code for a particular user, replacing this.context with the context of another user.
 
 server.listen().then(() => {
   console.log(`
