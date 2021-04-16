9f5d2af11712414a2a0acaed674ddda6cb42b327
Dylan Cortez-Modell <dylan.modell@gmail.com>
2021-04-16 15:29:40 -0700

    Part 1

--- working/server/src/index.js
+++ working/server/src/index.js
@@ -1,1 +1,15 @@
 require('dotenv').config();
+
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({typeDefs})
+
+server.listen().then(() => {
+  console.log(`
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/dev
+  `)
+});
+
