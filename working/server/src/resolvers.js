2c5452aed6723881d9afceab99d2015d1d5e8c1f
Dylan Cortez-Modell <dylan.modell@gmail.com>
2021-04-16 17:37:27 -0700

    step 3

---
+++ working/server/src/resolvers.js
@@ -0,0 +1,49 @@
+const { paginateResults } = require('./utils');
+
+module.exports = {
+  Query: {
+    launches: async (_, { pageSize = 20, after }, { dataSources }) =>
+    {
+      const allLaunches = await dataSources.launchAPI.getAllLaunches();
+      allLaunches.reverse();
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches
+      });
+      return {
+        launches,
+        cursor: launches.length ? launches[launches.length -1].cursor : null,
+        hasMore: launches.length 
+          ? launches[launches.length - 1].cursor !==
+            allLaunches[allLaunches.length -1 ].cursor
+          : false
+      }
+    },
+    launch: (_, { id }, { dataSources }) =>
+      dataSources.launchAPI.getLaunchById({ launchId: id }),
+    me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser(),
+  },
+  Mission: {
+    missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+      return size === 'SMALL'
+        ? mission.missionPatchSmall
+        : mission.missionPatchLarge;
+    },
+  },
+  Launch: {
+    isBooked: async (launch, _, { dataSources }) =>
+      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+  },
+  User: {
+    trips: async (_, __, { dataSources }) => {
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+      if (!launchedIds.length) return [];
+      return (
+        dataSources.launchAPI.getLaunchesByIds({
+          launchIds,
+        }) || []
+      );
+    },
+  },
+};
