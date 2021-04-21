3da3a58efb756802f8259699ac8742cc360a6c92
Dylan Cortez-Modell <dylan.modell@gmail.com>
2021-04-21 13:41:23 -0700

    step 6

--- working/client/src/cache.ts
+++ working/client/src/cache.ts
@@ -1,3 +1,5 @@
-import { InMemoryCache, Reference } from '@apollo/client';
+// import { InMemoryCache, Reference } from '@apollo/client';
+
+import { InMemoryCache } from '@apollo/client';
 
 export const cache: InMemoryCache = new InMemoryCache({});
