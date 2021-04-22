fe26fd0f910cddd8351b7c3b1d3ffc943934a557
Dylan Cortez-Modell <dylan.modell@gmail.com>
2021-04-22 09:40:44 -0700

    step 8

--- working/client/src/pages/login.tsx
+++ working/client/src/pages/login.tsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import React from "react";
+import { gql, useMutation } from "@apollo/client";
+
+import { LoginForm, Loading } from "../components";
+import * as LoginTypes from "./__generated__/login";
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!) {
+    login(email: $email) {
+      id
+      token
+    }
+  }
+`;
 
 export default function Login() {
-  return <div />;
+  const [login, { loading, error }] = useMutation<
+    LoginTypes.Login,
+    LoginTypes.LoginVariables
+  >(LOGIN_USER, {
+    onCompleted: ({ login }) => {
+      if (login) {
+        localStorage.setItem("token", login.token as string);
+        localStorage.setItem("userId", login.id as string);
+      }
+    },
+  });
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+
+  return <LoginForm login={login} />;
 }
